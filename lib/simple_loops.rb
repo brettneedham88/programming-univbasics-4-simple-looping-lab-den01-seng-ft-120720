b4c70010ad54875290ba14a3bc7f1ee42a629b1f
Brett Needham <brettneedham88@gmail.com>
2020-11-14 19:15:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,6 +7,14 @@ def loop_message_five_times(message)
   puts "Hello World."
 end
 
+def loop_message_n_times(message, limit)
+  count = 0
+  while count < limit do
+    puts message
+    count += 1
+  end
+end
+
 
  
 
