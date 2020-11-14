f39bbad5a74416942e4295453cff6b2b7f2afccb
Brett Needham <brettneedham88@gmail.com>
2020-11-14 19:12:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,8 +8,13 @@ def loop_message_five_times(message)
 end
 
 def output_array(array)
+  counter = 0
+ 
+  while array[counter] do
+    puts array[counter]
+    counter += 1
   
-array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
+  array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
 end
 
 output_array(array)
